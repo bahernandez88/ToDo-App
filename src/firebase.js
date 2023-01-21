2e76342ce50950876cafc528fa6b8fa198df8861
bhernandez-mycwi <b.a.hernandez@outlook.com>
2023-01-21 13:07:54 -0700

    Removed api key information
    Removed api key information to change repo from private to public

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0czZIieKjNdGrogykcSc6adP-xhfgr3I",
-  authDomain: "todo-list-54857.firebaseapp.com",
-  projectId: "todo-list-54857",
-  storageBucket: "todo-list-54857.appspot.com",
-  messagingSenderId: "530365881665",
-  appId: "1:530365881665:web:fd03365fb92a999b5ed5c0"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
